4def177765bc303e2222edb33b2d55cf3fe02f85
Alexander <get.sr@inbox.ru>
2022-04-04 23:04:45 +0300

    добавил первый скрипт

---
+++ Lesson_1/index.js
@@ -0,0 +1,18 @@
+console.log("Hello JAVASCRIPT!");
+const ourPlanetName = "Earth"
+let curentTime = "12^25"
+let articleCount = "25"
+let isCashOrNot = "yes"
+let firstName = "Ivan"
+let middlName = "Ivanovich"
+let lastName = "Ivanov"
+
+
+
+console.log(ourPlanetName)
+console.log(curentTime)
+console.log(articleCount)
+console.log(isCashOrNot)
+console.log(firstName)
+console.log(middlName)
+console.log(lastName)
